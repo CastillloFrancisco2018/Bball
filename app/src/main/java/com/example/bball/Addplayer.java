5940696caeb4c05c881ca5c22999fcad49b59737
franciscocastillo <fran.castillop@icloud.com>
2019-05-15 13:25:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bball/Addplayer.java
@@ -0,0 +1,34 @@
+package com.example.bball;
+
+import android.content.Intent;
+import android.widget.EditText;
+
+public class Addplayer {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_player);
+    }
+
+    public void onAddNewPlayerPressed(View v)
+    {
+        //change activity
+        Intent i = new Intent(this, MainActivity.class);
+        this.startActivity(i);
+
+        //EditTexts
+        EditText nameET = (EditText)this.findViewById(R.id.nameET);
+        EditText jerseyNumET = (EditText)this.findViewById(R.id.jerseyNumET);
+        EditText ageET = (EditText)this.findViewById(R.id.ageET);
+        EditText heightFeetET = (EditText)this.findViewById(R.id.heightFeetET);
+        EditText heightInchesET = (EditText)this.findViewById(R.id.heightInchesET);
+
+
+
+        //create a basketball Player                if the edit texts are empty the app crashes. add safeguard
+        basketballPlayer ballPlayer = new basketballPlayer(nameET.getText().toString(), Integer.parseInt(jerseyNumET.getText().toString()), Integer.parseInt(ageET.getText().toString()),
+                Integer.parseInt(heightFeetET.getText().toString()), Integer.parseInt(heightInchesET.getText().toString()));
+
+        ballPlayer.displayPlayer();
+    }
+}
---
+++ app/src/main/java/com/example/bball/Player.java
@@ -0,0 +1,52 @@
+package com.example.bball;
+
+public class Player {
+    private String name;
+    private int jerseyNum;
+    private int age;
+    private int heightFeet;
+    private int heightInches;
+
+    //Constructor
+    public Player(String name, int jerseyNum, int age, int heightFeet, int heightInches)
+    {
+        this.name = name;
+        this.jerseyNum = jerseyNum;
+        this.age = age;
+        this.heightFeet = heightFeet;
+        this.heightInches = heightInches;
+    }
+
+    //Methods
+    public void displayPlayer()
+    {
+        System.out.format("%s is number %d. And at the age of %d years old, he/she stands %d feet and %d inches tall.\n",
+                this.name, this.jerseyNum, this.age, this.heightFeet, this.heightInches);
+    }
+
+    //Getters
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public int getJerseyNum()
+    {
+        return this.jerseyNum;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public int getHeightFeet()
+    {
+        return this.heightFeet;
+    }
+
+    public int getHeightInches()
+    {
+        return this.heightInches;
+    }
+}
